79a7879454b215ce1825a87daaaf4f12fa2c51b5
Mauricio <mauricioguitart@gmail.com>
2023-09-13 20:39:27 -0300

    comit-03

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="Name change" content="width=device-width, initial-scale=1.0">
-        <title>HTML Elements Reference</title>
+        <title>titulo</title>
     </head>
     <body>
         <h1>Heading</h1>
