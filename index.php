e6572d640a7db9dde69af4c7512ef2624886bba5
Mau <mauricioguitart@gmail.com>
2023-09-26 20:58:45 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,53 +1,68 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="Name chansrege" content="width=device-width, initial-scale=1.0">
-        <title>titulo</title>
-    </head>
-    <body>
-        <h1>Heading</h1>
-        <p>
-            Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..
-            Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..
-            Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupisssssdatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..
-        </p>
-        <p>
-        Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..
-            Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..
-            Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupisssssdatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..
-        </p>
-         <p>
-        Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..
-            Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..
-            Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupisssssdatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..
-        </p>
-    </body>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="" content="width = device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title></title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <nav class="encabezado">
+                <img src="img/mcdonalds-logo-footer-bg-white.png" alt="icono de la homepage"> 
+                <div class="links_de_navegacion">
+                    <div class="row encabezado_alto">
+                        <div class="column traba_etc">
+                            <li>Trabaja con Nosotros</li>
+                        </div>
+                        <div class="column">
+                            <li>Descarga la app</li>
+                        </div>
+                        <div class="column">
+                            <li>Pedi y retira</li>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="column">
+                            <li>Productos</li> 
+                        </div>
+                        <div class="column">
+                            <li>Lanzamientos</li>
+                        </div>
+                        <div class="column">
+                            <li>Locales</li>
+                        </div>
+                        <div class="column">
+                            <li>En Familia</li>
+                        </div>
+                        <div class="column">
+                            <li>Nosotros</li>
+                        </div>
+                    </div>
+                </div>
+               
+
+            </nav>
+        </div>
+        <br><br><br>
+        <div class="contenedor cuerpo">
+            <img src="img/Banner_APP_WEB_piletacheddar_1440_X650_WEB_DESKTOP_87c29c5063.jpg" alt="mcdonald's">
+        </div>
+        
+    </header>
+    <br>
+    <br><br><br>
+    <footer>
+        <div class="pie de pagina">
+        <li>Política de privacidad</li>
+        <li>Defensa del consumidor</li>
+        <li>Protección de datos personales</li>
+        <li>Contacto</li>
+        <li>© McDonald's 2023</li>
+    </div>
+    </footer>
+
+</body>
 </html>
